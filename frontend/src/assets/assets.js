9b454a9d373c37f25f49b838f733b2ca4d7727f7
Pratibha Bagewadi <pratibhabagewadi1@gmail.com>
2025-09-11 15:17:54 +0530

    Fixs

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,35 +1,35 @@
-import logo from './logo.jpg';
-import user_icon from './user_icon.jpg';
-import search_icon from './search_icon.jpg'; 
-import microsoft_logo from './microsoft_logo.jpg';
-import walmart_logo from './walmart_logo.jpg'; 
-import paypal_logo from './paypal_logo.jpg';
-import accenture_logo from './accenture_logo.jpg'; 
-import adobe_logo from './adobe_logo.jpg';
-import star from './star.jpg';   
-import node_course from './node_course.jpg';
-import star_blank from './star_blank.jpg';
-import arrow_icon from './arrow_icon.jpg';
-import cross_icon from './cross_icon.jpg';
-import down_arrow_icon from './down_arrow_icon.jpg';
-import play_icon from './play_icon.jpg';
-import time_left_clock_icon from './time_left_clock_icon.jpg';
-import time_clock_icon from './time_clock_icon.jpg';
-import lession_icon from './lession_icon.jpg';
-import blue_tick_icon from './blue_tick_icon.jpg';
-import home_icon from './home_icon.jpg';
-import add_icon from './add_icon.jpg';
-import my_course_icon from './my_course_icon.jpg';
-import person_tick_icon from './person_tick_icon.jpg';
-import instagram_icon from './instagram_icon.jpg';
-import facebook_icon from './facebook_icon.jpg';
-import twitter_icon from './twitter_icon.jpg';
-import patients_icon from './patients_icon.jpg';
-import appointment_icon from './appointment_icon.jpg';
-import earning_icon from './earning_icon.jpg';
-import addQuiz_icon from './addQuiz_icon.jpg';
-import quiz_icon from './quiz_icon.jpg'; 
-import messages_icon from './messages_icon.jpg'
+import logo from "./logo.jpg";
+import user_icon from "./user_icon.jpg";
+import search_icon from "./search_icon.jpg";
+import microsoft_logo from "./microsoft_logo.jpg";
+import walmart_logo from "./walmart_logo.jpg";
+import paypal_logo from "./paypal_logo.jpg";
+import accenture_logo from "./accenture_logo.jpg";
+import adobe_logo from "./adobe_logo.jpg";
+import star from "./star.jpg";
+import node_course from "./node_course.jpg";
+import star_blank from "./star_blank.jpg";
+import arrow_icon from "./arrow_icon.jpg";
+import cross_icon from "./cross_icon.jpg";
+import down_arrow_icon from "./down_arrow_icon.jpg";
+import play_icon from "./play_icon.jpg";
+import time_left_clock_icon from "./time_left_clock_icon.jpg";
+import time_clock_icon from "./time_clock_icon.jpg";
+import lession_icon from "./lession_icon.jpg";
+import blue_tick_icon from "./blue_tick_icon.jpg";
+import home_icon from "./home_icon.jpg";
+import add_icon from "./add_icon.jpg";
+import my_course_icon from "./my_course_icon.jpg";
+import person_tick_icon from "./person_tick_icon.jpg";
+import instagram_icon from "./instagram_icon.jpg";
+import facebook_icon from "./facebook_icon.jpg";
+import twitter_icon from "./twitter_icon.jpg";
+import patients_icon from "./patients_icon.jpg";
+import appointment_icon from "./appointment_icon.jpg";
+import earning_icon from "./earning_icon.jpg";
+import addQuiz_icon from "./addQuiz_icon.jpg";
+import quiz_icon from "./quiz_icon.jpg";
+import messages_icon from "./messages_icon.jpg";
 
 export const assets = {
   logo,
@@ -63,5 +63,5 @@ export const assets = {
   earning_icon,
   addQuiz_icon,
   quiz_icon,
-  messages_icon
+  messages_icon,
 };
