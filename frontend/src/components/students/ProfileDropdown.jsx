718c5edac5cd9c1b77bbe900d24e6cafd38f2bc5
Pratibha Bagewadi <pratibhabagewadi1@gmail.com>
2025-09-15 18:00:04 +0530

    change url

--- frontend/src/components/students/ProfileDropdown.jsx
+++ frontend/src/components/students/ProfileDropdown.jsx
@@ -98,7 +98,7 @@ const ProfileDropdown = () => {
         <img
   src={
     user?.profileImage
-      ? `http://13.233.183.81${user.profileImage}`
+      ? `http://13.233.183.81:5000${user.profileImage}`
       : assets.user_icon || "/default-avatar.png"
   }
   alt="profile"
